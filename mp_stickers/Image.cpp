6e26f571c9634b1fb6089953e62fb0f70321a9f7
shubhg3 <shubhg3@illinois.edu>
2022-01-30 00:00:46 +0000

    Started Mp1

---
+++ mp_stickers/Image.cpp
@@ -0,0 +1,11 @@
+#pragma
+#include "cs225/PNG.h"
+#include "image.h"
+
+namespace cs225{
+    void image::darken(){
+        unsigned height = this->height();
+        unsigned width= this->width();
+    }
+
+}
